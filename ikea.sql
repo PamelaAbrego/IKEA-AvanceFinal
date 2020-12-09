a5b60459a02adfa531554f098d7a5211516bdb10
PamelaAbrego <71997534+PamelaAbrego@users.noreply.github.com>
2020-12-08 18:45:23 -0600

    Add files via upload

--- ikea.sql
+++ ikea.sql
@@ -35,7 +35,7 @@ CREATE TABLE `carritosdecompras` (
   KEY `carritos-usuarios_idx` (`idUsuario`),
   CONSTRAINT `carritos-productos` FOREIGN KEY (`idProducto`) REFERENCES `productos` (`id`),
   CONSTRAINT `carritos-usuarios` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -44,7 +44,7 @@ CREATE TABLE `carritosdecompras` (
 
 LOCK TABLES `carritosdecompras` WRITE;
 /*!40000 ALTER TABLE `carritosdecompras` DISABLE KEYS */;
-INSERT INTO `carritosdecompras` VALUES (1,1,1,1,1),(2,2,2,3,2),(3,3,3,4,1),(4,1,1,2,2),(5,2,2,1,1),(6,3,3,2,2),(7,1,1,1,1);
+INSERT INTO `carritosdecompras` VALUES (1,1,1,1,1),(2,2,2,3,2),(3,3,3,4,1),(4,1,1,2,2),(5,2,2,1,1),(6,3,3,2,2),(20,4,36,1,1),(21,5,1,2,1),(22,5,1,1,1),(25,6,1,1,1),(26,6,1,1,1),(27,6,1,1,1),(28,7,1,16,10),(29,7,1,1,5);
 /*!40000 ALTER TABLE `carritosdecompras` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -127,35 +127,6 @@ INSERT INTO `claseproductos` VALUES (1,'Mesas',1),(2,'Sillones',1),(3,'Camas',2)
 UNLOCK TABLES;
 
 --
--- Table structure for table `entregas`
---
-
-DROP TABLE IF EXISTS `entregas`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `entregas` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `idFacturas` int NOT NULL,
-  `fecha` date NOT NULL,
-  `hora` time NOT NULL,
-  `lugar` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `entregas-facturas_idx` (`idFacturas`),
-  CONSTRAINT `entregas-facturas` FOREIGN KEY (`idFacturas`) REFERENCES `facturas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `entregas`
---
-
-LOCK TABLES `entregas` WRITE;
-/*!40000 ALTER TABLE `entregas` DISABLE KEYS */;
-INSERT INTO `entregas` VALUES (1,1,'2020-09-10','08:30:00','Casa'),(2,2,'2020-06-26','08:30:00','Casa'),(3,3,'2020-08-14','08:30:00','Oficina'),(4,1,'2020-10-03','08:30:00','Oficina');
-/*!40000 ALTER TABLE `entregas` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `existencias`
 --
 
@@ -172,7 +143,7 @@ CREATE TABLE `existencias` (
   KEY `existencias-sucursales_idx` (`idSucursales`),
   CONSTRAINT `existencias-productos` FOREIGN KEY (`idProductos`) REFERENCES `productos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `existencias-sucursales` FOREIGN KEY (`idSucursales`) REFERENCES `sucursales` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=67 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -181,7 +152,7 @@ CREATE TABLE `existencias` (
 
 LOCK TABLES `existencias` WRITE;
 /*!40000 ALTER TABLE `existencias` DISABLE KEYS */;
-INSERT INTO `existencias` VALUES (1,1,1,10),(2,2,2,15),(3,3,3,25),(4,4,4,6),(5,1,5,12),(6,2,1,22),(9,1,4,5);
+INSERT INTO `existencias` VALUES (13,1,1,20),(14,1,2,10),(15,1,3,10),(16,1,4,10),(17,1,5,5),(18,20,1,5),(19,2,1,5),(20,3,1,5),(21,4,1,12),(22,15,1,50),(23,16,1,23),(24,17,1,45),(25,18,1,65),(26,19,1,12),(27,1,2,23),(28,2,2,12),(29,3,2,32),(30,4,2,41),(31,15,2,23),(32,16,2,12),(33,17,2,32),(34,18,2,11),(35,19,2,10),(36,20,2,3),(37,1,3,43),(38,2,3,25),(39,3,3,63),(40,4,3,82),(41,15,3,4),(42,16,3,6),(43,17,3,31),(44,18,3,43),(45,19,3,12),(46,20,3,31),(47,1,4,1),(48,2,4,23),(49,3,4,12),(50,4,4,42),(51,15,4,31),(52,16,4,53),(53,17,4,12),(54,18,4,31),(55,19,4,2),(56,20,4,1),(57,1,5,5),(58,2,5,34),(59,3,5,54),(60,4,5,23),(61,15,5,12),(62,16,5,33),(63,17,5,12),(64,18,5,43),(65,19,5,10),(66,20,5,3);
 /*!40000 ALTER TABLE `existencias` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -206,7 +177,7 @@ CREATE TABLE `facturas` (
   KEY `facturas-sucursales_idx` (`idSucursales`),
   CONSTRAINT `facturas-sucursales` FOREIGN KEY (`idSucursales`) REFERENCES `sucursales` (`id`),
   CONSTRAINT `facturas-usuarios` FOREIGN KEY (`idUsuarios`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -215,7 +186,7 @@ CREATE TABLE `facturas` (
 
 LOCK TABLES `facturas` WRITE;
 /*!40000 ALTER TABLE `facturas` DISABLE KEYS */;
-INSERT INTO `facturas` VALUES (1,1,'Efectivo',1,'2020-06-14','08:30:00',1,350),(2,2,'Efectivo',2,'2020-08-24','14:00:00',2,325),(3,3,'Efectivo',3,'2020-11-09','16:00:35',3,130),(4,1,'Tarjeta',4,'2020-04-02','08:45:22',1,350),(5,2,'Tarjeta',1,'2020-06-12','06:30:00',2,325),(6,3,'Tarjeta',5,'2020-09-24','22:30:00',3,130);
+INSERT INTO `facturas` VALUES (1,1,'Efectivo',1,'2020-06-14','08:30:00',1,350),(2,2,'Efectivo',2,'2020-08-24','14:00:00',2,325),(3,3,'Efectivo',3,'2020-11-09','16:00:35',3,130),(4,1,'Tarjeta',4,'2020-04-02','08:45:22',1,350),(5,2,'Tarjeta',1,'2020-06-12','06:30:00',2,325),(6,3,'Tarjeta',5,'2020-09-24','22:30:00',3,130),(14,36,'Efectivo',1,'2020-12-08','16:04:06',4,125),(15,1,'Cheque',5,'2020-12-08','18:29:55',7,1225);
 /*!40000 ALTER TABLE `facturas` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -263,7 +234,7 @@ CREATE TABLE `productos` (
   PRIMARY KEY (`id`),
   KEY `prodcutos-clase_idx` (`idClaseProductos`),
   CONSTRAINT `prodcutos-clase` FOREIGN KEY (`idClaseProductos`) REFERENCES `claseproductos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -272,7 +243,7 @@ CREATE TABLE `productos` (
 
 LOCK TABLES `productos` WRITE;
 /*!40000 ALTER TABLE `productos` DISABLE KEYS */;
-INSERT INTO `productos` VALUES (1,'Mesa',125,'1.20x2.00x1.00 [m]','Madera','Negro, Blanco, Café','Modelo ME-01','1 año',1),(2,'Sillon',50,'0.70x1.00x1.00 [m]','Cuero','Negro, Blanco','Modelo SI-01','6 meses',2),(3,'Cama',100,'1.30x2.00x0.5 [m]','Espuma','Blanco, Beige','Model CA-01','1 año',3),(4,'Espejo',30,'radio 0.15 [m]','Vidrio','Blanco, Negro','Modelo ES-01','2 semanas',4);
+INSERT INTO `productos` VALUES (1,'Mesa',125,'1.20x2.00x1.00 [m]','Madera','Negro','Modelo ME-01','1 año',1),(2,'Sillon',50,'0.70x1.00x1.00 [m]','Cuero','Blanco','Modelo SI-01','6 meses',2),(3,'Cama',100,'1.30x2.00x0.5 [m]','Espuma','Beige','Model CA-01','1 año',3),(4,'Espejo',30,'radio 0.15 [m]','Vidrio','Negro','Modelo ES-01','2 semanas',4),(15,'Florero',25,'0.02*0.03*0.01 [m]','Cerámica','Blanco','Modelo FL-01','2 meses',5),(16,'Escritorio',60,'0.50*1.00*0.50 [m]','Madera','Café','Modelo ES-01','1 año',1),(17,'Ducha',25,'N/A','Aluminio','Gris','Modelo DU-01','3 meses',6),(18,'Lampara',45,'2.00*0.01*0.01 [m]','Metal','Rojo','Modelo LA-01','5 meses',5),(19,'Cocina',500,'1.00*0.75*0.75 [m]','Metal','Blanco','Modelo CO-01','1 año',7),(20,'Estantería',70,'2.00*0.5*0.3 [m]','Madera','Azul','Modelo EST-01','4 meses',1);
 /*!40000 ALTER TABLE `productos` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -324,7 +295,7 @@ CREATE TABLE `usuarios` (
   `contrasenna` varchar(45) NOT NULL,
   `direccion` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -333,7 +304,7 @@ CREATE TABLE `usuarios` (
 
 LOCK TABLES `usuarios` WRITE;
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-INSERT INTO `usuarios` VALUES (1,'pamelaabrego','Pamela','Abrego','Ramirez','7658-6598','Español','pamela@gmail.com','pamela1','San Salvador'),(2,'franciscofuentes','Francisco','Fuentes','--','7549-9246','Español','francisco@gmail.com','francisco1','San Miguel'),(3,'javierosorio','Javier','Osorio','Echeverria','7490-2486','Ingles','javier@gmail.com','javier1','San Salvador'),(32,'as','as','as','as','as','as','as','as','as');
+INSERT INTO `usuarios` VALUES (1,'pamelaabrego','Pamela','Abrego','Ramirez','7658-6598','Español','pamela@gmail.com','pamela1','San Salvador'),(2,'franciscofuentes','Francisco','Fuentes','--','7549-9246','Español','francisco@gmail.com','francisco1','San Miguel'),(3,'javierosorio','Javier','Osorio','Echeverria','7490-2486','Ingles','javier@gmail.com','javier1','San Salvador'),(36,'adriabrego','Adri','Abrego','Ramirez','2345678','Español','adri@gmail.com','adri1','San Salvador');
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -346,4 +317,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-12-07 22:46:57
+-- Dump completed on 2020-12-08 18:45:05
